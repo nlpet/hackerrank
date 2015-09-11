ad3cdbb0b50f0feb6cae073c9f884cd3a3a40e3e
Nora Petrova <schottkey@gmail.com>
2015-09-11 17:43:08 +0100

    Create matrix_rotation.py
    Initial notes about the problem

---
+++ Algorithms/Implementation/matrix_rotation.py
@@ -0,0 +1,60 @@
+"""
+Matrix RotatioN: https://www.hackerrank.com/challenges/matrix-rotation-algo
+
+[[1,2,3,4],
+[5,6,7,8],
+[9,10,11,12],
+[13,14,15,16]]
+
+6 rotations:
+ (0,0) => (3,3)
+
+27 rotations:
+ (0,0) => (3,0) (26 % 12 = 3 rotations, x + 3)
+ 
+first cycle: x -> [0, M - 1]
+second cycle: x = M - 1
+third cycle: x -> [M - 1, 0]
+fourth cycle: x = 0
+
+first cycle: y -> 0
+second cycle: y -> +1
+third cycle: y -> +1
+fourth cycle: y -> +1
+
+indices = (0, 0)
+
+flag = first
+
+for i in range(rotations):
+    if flag == first
+        x ++
+    elif flag == second
+        y ++
+    elif flag = third
+        x --
+    else
+        y --
+        
+ 
+"""
+
+# Num rows
+M = 4
+
+# Num columns
+N = 4
+
+# Rotations
+R = 1
+
+
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
+cycle = N * M - 4
+rotations = R % wrap
+
+index = (0,0)
+reverse = {'row' : 1, 'col': 1}
+
+
