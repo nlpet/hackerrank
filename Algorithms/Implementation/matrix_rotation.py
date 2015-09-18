9edcb60edc82d896a74e5e11ae4406b9a929c050
Nora Petrova <schottkey@gmail.com>
2015-09-18 15:32:12 +0100

    Update matrix_rotation.py
    More progress

--- Algorithms/Implementation/matrix_rotation.py
+++ Algorithms/Implementation/matrix_rotation.py
@@ -1,66 +1,45 @@
 """
 Matrix RotatioN: https://www.hackerrank.com/challenges/matrix-rotation-algo
-
-[[1,2,3,4],
-[5,6,7,8],
-[9,10,11,12],
-[13,14,15,16]]
-
-6 rotations:
- (0,0) => (3,3)
-
-27 rotations:
- (0,0) => (3,0) (26 % 12 = 3 rotations, x + 3)
- 
-first cycle: x -> [0, M - 1]
-second cycle: x = M - 1
-third cycle: x -> [M - 1, 0]
-fourth cycle: x = 0
-
-first cycle: y -> 0
-second cycle: y -> +1
-third cycle: y -> +1
-fourth cycle: y -> +1
-
-x,y = (0, 0)
-
-flag = 1
-
-for i in range(rotations):
-       
-    if flag == first
-        x ++
-    elif flag == second
-        y ++
-    elif flag = third
-        x --
-    else
-        y --
-        
-    if x == M - 1 and y == 0: flag 2
-    elif x == M - 1 and y == N - 1: flag 3
-    elif x == 0 and y == N - 1: flag 4
-    else: flag 1
-        
  
 """
 
-# Num rows
-M = 4
+def unroll_matrix(matrix, M, N):
+    rings = M // 2
+    unrolled = [[] for _ in range(rings)]
+    for ring in range(rings):
+        top, left_strip, right_strip, bottom = [], [], [], []
+        for y in range(ring, M - ring):
+            for x in range(ring, N - ring):
+                if y == ring:
+                    top = matrix[y][ring: N - ring]
+                    break
+                elif y == (M - ring - 1):
+                    bottom = matrix[y][ring: N - ring]
+                    break
+                else:
+                    left_strip.append(matrix[y][x])
+                    right_strip.append(matrix[y][N - ring - 1])
+                    break
+        unrolled[ring] = top + right_strip + bottom[::-1] + left_strip[::-1]
+    return unrolled
+                
 
-# Num columns
-N = 4
+if __name__ == '__main__':
+    matrix = [[1, 2, 3, 4],
+              [5, 6, 7, 8],
+              [9, 10, 11, 12],
+              [13, 14, 15, 16]]
 
-# Rotations
-R = 1
+    # Num rows
+    M = 4
 
+    # Num columns
+    N = 4
 
-matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+    # Rotations
+    R = 1
 
-cycle = N * M - 4
-rotations = R % wrap
+    cycle = N * M - 4
+    rotations = R % cycle
 
-index = (0,0)
-reverse = {'row' : 1, 'col': 1}
-
-
+    unrolled = unroll_matrix(matrix, M, N)
