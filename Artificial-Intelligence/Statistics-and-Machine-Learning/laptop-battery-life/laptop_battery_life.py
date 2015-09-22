00e47030d501cb3a4ee5226c86df39e2a14bdd44
Nora Petrova <schottkey@gmail.com>
2015-09-22 22:59:21 +0100

    Create laptop_battery_life.py

---
+++ Artificial-Intelligence/Statistics-and-Machine-Learning/laptop-battery-life/laptop_battery_life.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+data = float(sys.stdin.readline())
+
+result = data * 2
+if result > 8.00:
+    print 8.00
+else:
+    print result
