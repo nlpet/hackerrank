6597016756c64854a98974a749220cc353dca48c
schottkey <schottkey@gmail.com>
2015-09-26 17:51:53 +0100

    updates

--- Algorithms/Implementation/matrix_rotation.py
+++ Algorithms/Implementation/matrix_rotation.py
@@ -1,28 +1,55 @@
 """
 Matrix RotatioN: https://www.hackerrank.com/challenges/matrix-rotation-algo
- 
 """
 
-def unroll_matrix(matrix, M, N):
-    rings = M // 2
+
+def unroll_matrix(matrix, M, N, rings):
     unrolled = [[] for _ in range(rings)]
-    for ring in range(rings):
+    for r in range(rings):
         top, left_strip, right_strip, bottom = [], [], [], []
-        for y in range(ring, M - ring):
-            for x in range(ring, N - ring):
-                if y == ring:
-                    top = matrix[y][ring: N - ring]
+        # Iterate over columns
+        for y in range(r, M - r):
+            # Iterate over rows
+            for x in range(r, N - r):
+                if y == r:
+                    top = matrix[y][r: N - r]
                     break
-                elif y == (M - ring - 1):
-                    bottom = matrix[y][ring: N - ring]
+                elif y == (M - r - 1):
+                    bottom = matrix[y][r: N - r]
                     break
                 else:
                     left_strip.append(matrix[y][x])
-                    right_strip.append(matrix[y][N - ring - 1])
+                    right_strip.append(matrix[y][N - r - 1])
                     break
-        unrolled[ring] = top + right_strip + bottom[::-1] + left_strip[::-1]
+        unrolled[r] = top + right_strip + bottom[::-1] + left_strip[::-1]
     return unrolled
-                
+
+
+def rotate(unrolled, rotations):
+    for i in range(len(unrolled)):
+        unrolled[i] = unrolled[i][rotations:] + unrolled[i][:rotations]
+
+
+def roll_matrix(unrolled, M, N, rings):
+    matrix = [[None] * N for _ in range(M)]
+    for r in range(rings):
+        if len(matrix[r]) == 4:
+            top = matrix[r][:2]
+            left_strip = []
+            bottom = matrix[r][2:]
+            right_strip = []
+        else:
+            top = matrix[r][: (N - r)]
+            left_strip = matrix[r][(N - r): (N - r) + (M - r) - 2]
+            bottom = matrix[r][(N - r) + (M - r) - 2: 2 * (N - r) + (M - r) - 2]
+            right_strip = matrix[r][2 * (N - r) + (M - r) - 2:]
+        # Iterate over columns
+        for y in range(r, M - r):
+            # Iterate over rows
+            for x in range(r, N - r):
+                pass
+
+
 
 if __name__ == '__main__':
     matrix = [[1, 2, 3, 4],
@@ -39,7 +66,10 @@ if __name__ == '__main__':
     # Rotations
     R = 1
 
+    # Rings
+    rings = M // 2
+
     cycle = N * M - 4
     rotations = R % cycle
 
-    unrolled = unroll_matrix(matrix, M, N)
+    unrolled = unroll_matrix(matrix, M, N, rings)
