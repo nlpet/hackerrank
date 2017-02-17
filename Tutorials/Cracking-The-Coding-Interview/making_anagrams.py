a0df0bf7142993f9ce3be60f84b74e887bc689e5
schottkey <schottkey@gmail.com>
2017-02-17 20:52:22 +0000

    small fix

--- Tutorials/Cracking-The-Coding-Interview/making_anagrams.py
+++ Tutorials/Cracking-The-Coding-Interview/making_anagrams.py
@@ -5,8 +5,7 @@ from collections import Counter
 def number_needed(a, b):
     alterations = 0
     ca, cb = Counter(a), Counter(b)
-    sa, sb = set(a), set(b)
-    a_b_int = sa.intersection(sb)
+    a_b_int = set(a).intersection(set(b))
 
     for k in a_b_int:
         diff = int(fabs(ca.pop(k) - cb.pop(k)))
