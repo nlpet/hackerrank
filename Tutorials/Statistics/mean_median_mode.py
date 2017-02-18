6f8a565c70a5d802793e9183ccfc592ea09ba314
schottkey <schottkey@gmail.com>
2017-02-18 14:59:41 +0000

    added statistics exercise

---
+++ Tutorials/Statistics/mean_median_mode.py
@@ -0,0 +1,27 @@
+from collections import Counter, defaultdict
+
+
+def mean(arr, n):
+    return sum(arr) / n
+
+
+def median(arr, n):
+    sorted_arr = sorted(arr)
+    if n % 2 == 0:
+        return (sorted_arr[(n - 1) // 2] + sorted_arr[n // 2]) / 2
+    else:
+        return sorted_arr[n // 2]
+
+
+def mode(arr):
+    ca = Counter(arr).most_common()
+    max_occ = ca[0][1]
+    return min(list(filter(lambda t: t[1] == max_occ, ca)), key=lambda t: t[0])[0]
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    print(mean(arr, n))
+    print(median(arr, n))
+    print(mode(arr))
