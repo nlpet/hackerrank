ff39004f8a27ce51a61da7bbe2c0eece7ed48e3e
Nora Petrova <schottkey@gmail.com>
2017-02-24 14:26:54 +0000

    Started on the central limit theorem problem

---
+++ Tutorials/Statistics/central_limit_theorem.py
@@ -0,0 +1,5 @@
+# Central Limit Theorem
+# The central limit theorem (CLT) states that, for a large enough sample (n)
+# the distribution of the sample mean will approach normal distribution.
+# This holds for a sample of independent random variables from any distribution
+# with a finite standard deviation.
