15691de26235bc0252aac17bd0e9555839cd91ca
Nora Petrova <schottkey@gmail.com>
2017-02-24 21:31:25 +0000

    Added central limit theorem exercise

--- Tutorials/Statistics/central_limit_theorem.py
+++ Tutorials/Statistics/central_limit_theorem.py
@@ -1,5 +1,21 @@
+from math import sqrt, erf
+
 # Central Limit Theorem
 # The central limit theorem (CLT) states that, for a large enough sample (n)
 # the distribution of the sample mean will approach normal distribution.
 # This holds for a sample of independent random variables from any distribution
 # with a finite standard deviation.
+# https://www.hackerrank.com/challenges/s10-the-central-limit-theorem-1
+
+def cdf(mu, std, x):
+    return 0.5 * (1 + erf((x - mu) / (std * sqrt(2))))
+
+
+if __name__ == '__main__':
+    maxn = 9800
+    n = 49
+    mu = 205
+    std = 15
+    mup = n * mu
+    stdp = sqrt(n) * std
+    print('{:.4f}'.format(cdf(mup, stdp, maxn)))
